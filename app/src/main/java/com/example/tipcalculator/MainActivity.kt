ea1951e8b49c04d78eb784a58606f79429315263
semihcansertbas <semihcansertbas@gmail.com>
2021-06-24 01:22:55 -0500

    CalculationInvoke

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -2,11 +2,39 @@ package com.example.tipcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        totalText.visibility = View.INVISIBLE
+        totalTipText.visibility = View.INVISIBLE
+
+        calculateButton.setOnClickListener {
+            // Bill
+            val bill = billAmountNumberEdit.text.toString().toDouble()
+
+            // Tip Percentage
+            val tipPercentage = tipPercentageNumberEdit.text.toString().toDouble() / 100
+
+            // Tip
+            val tip = bill * tipPercentage
+
+            // Total
+            val total = bill + tip
+
+            // Make Total's visible
+            totalText.visibility = View.VISIBLE
+            totalText.text = "Total: \$${total}"
+            totalTipText.visibility = View.VISIBLE
+            totalTipText.text = "Tip: \$${tip}"
+
+
+        }
 
 
     }
